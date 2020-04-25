534675a112c0ccc10ca6d1d160ff5a6085be7623
Prafulla Joshi <joshiprafulla21@gmail.com>
2020-04-25 13:44:15 +0530

    CRUD functionality implemented in Spring-Boot

--- Todo-Management-System/Back-end/restful-web-services/src/main/java/com/prafulla/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ Todo-Management-System/Back-end/restful-web-services/src/main/java/com/prafulla/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -2,6 +2,7 @@ package com.prafulla.webservices.restfulwebservices.todo;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -22,5 +23,45 @@ public class TodoHardcodedService
 	public List<Todo> findAll()
 	{
 		return todos;
+	}
+	
+	public Todo deleteById(long id)
+	{
+		Todo todo = findById(id);
+		if(todo == null)
+			return null;
+		if(todos.remove(todo))
+		{
+			return todo;
+		}
+		return null;
+	}
+
+	public Todo findById(long id)
+	{
+		for(Todo todo : todos)
+		{
+			if(todo.getId() == id)
+			{
+				return todo;
+			}
+		}
+		return null;
+	}
+	
+	public Todo save(Todo todo)
+	{
+		if (todo.getId() == -1 || todo.getId() == 0)
+		{
+			// insert
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else
+		{
+			// update
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
 	}
 }
--- Todo-Management-System/Back-end/restful-web-services/src/main/java/com/prafulla/webservices/restfulwebservices/todo/TodoResource.java
+++ Todo-Management-System/Back-end/restful-web-services/src/main/java/com/prafulla/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,14 +1,20 @@
 package com.prafulla.webservices.restfulwebservices.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.prafulla.webservices.restfulwebservices.todo.Todo;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -22,4 +28,45 @@ public class TodoResource
 	{
 		return todoService.findAll();
 	}
+	
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id)
+	{
+		return todoService.findById(id);
+	}
+	
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id)
+	{
+		Todo todo = todoService.deleteById(id);
+		if (todo != null)
+		{
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, 
+			@PathVariable long id, @RequestBody Todo todo)
+	{
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> updateTodo(@PathVariable String username, 
+			@RequestBody Todo todo)
+	{
+		Todo todoCreated = todoService.save(todo);
+		
+		// Location
+		// Get current resource URL
+		// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(todoCreated.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
