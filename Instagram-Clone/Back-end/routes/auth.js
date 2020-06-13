63ff52e5d8876b5fbef92d37b8fa65658cdebcad
Prafulla Joshi <joshiprafulla21@gmail.com>
2020-06-13 21:57:08 +0530

    Signup & Signin Routing Done with Password Hashing

--- Instagram-Clone/Back-end/routes/auth.js
+++ Instagram-Clone/Back-end/routes/auth.js
@@ -3,37 +3,65 @@ const { Router } = require("express");
 const ROUTER = EXPRESS.Router();
 const MONGOOSE = require(`mongoose`);
 const USER = MONGOOSE.model("User");
+const BCRYPT = require(`bcryptjs`);         // For hashing the password in MongoDB
 
-ROUTER.get(`/`,(req, res) => {
-    res.send(`Hello`);
-})
-
-ROUTER.post(`/signup`,(req, res) => {
-    const {name, email, password} = req.body;
+ROUTER.post(`/signup`, (req, res) => {
+    const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        return res.status(422).json({error : `Please add all the fields!`});
+        return res.status(422).json({ error: `Please add all the fields!` });
     }
-    USER.findOne({email:email})
-    .then((savedUser) => {
-        if (savedUser) {
-            return res.status(422).json({error : `User already exists with that email!`});
-        }
-        const user = new USER({
-            email,
-            password,
-            name
-        })
-        user.save()
-        .then(user => {
-            res.status(200).json({message : `Saved Successfully!!`});
+    USER.findOne({ email: email })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: `User already exists with that email!` });
+            }
+            BCRYPT.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new USER({
+                        email,
+                        password: hashedPassword,
+                        name
+                    })
+                    user.save()
+                        .then(user => {
+                            res.status(200).json({ message: `Saved Successfully!!` });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        })
+                })
         })
         .catch(err => {
             console.log(err);
         })
-    })
-    .catch(err => {
-        console.log(err);
-    })
+})
+
+ROUTER.post(`/signin`,(req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(422).json({error : `Please add email or password`});
+    }
+    USER.findOne({email:email})
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({error:`Invalid email or password`});
+            }
+            BCRYPT.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        res.json({message: `Successfully Signed In`});
+                    }
+                    else{
+                        return res.status(422).json({error:`Invalid email or password`});
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
 
