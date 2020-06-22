82a291b436a9bb1bebcc1c0c49f4b1f56d201b33
Prafulla Joshi <joshiprafulla21@gmail.com>
2020-06-22 21:03:10 +0530

    Unnecessary code removed

--- Cab-Booking-Service/Back-end/cab-booking/migrations/migrate-mongo-config.js
+++ Cab-Booking-Service/Back-end/cab-booking/migrations/migrate-mongo-config.js
@@ -6,7 +6,7 @@ const config = {
     url: "mongodb://localhost:27017",
 
     // TODO Change this to your database name:
-    databaseName: "YOURDATABASENAME",
+    databaseName: "cbs_db",
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
